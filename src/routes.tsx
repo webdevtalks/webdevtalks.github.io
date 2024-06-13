a2b1fd65448fe57654e6ee6b8ada3d0094c92f6e
Miguel Urbina <itismeurbi@gmail.com>
2024-06-13 12:14:16 -0600

    Create about component

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import { useRoutes } from "react-router-dom";
 import { type ReactElement } from 'react'
 import Home from './Home'
 import Sponsorship from './Sponsorship'
+import About from './About'
 
 export default function Router (): ReactElement | null {
   const routes = useRoutes([
     { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
     { path: '/sponsorship', element: <Sponsorship /> },
   ])
 
