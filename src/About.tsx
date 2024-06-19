d7eccce819bbfcc8596e63d6f1f93de6f258ae88
Miguel Urbina <itismeurbi@gmail.com>
2024-06-18 18:08:49 -0600

    Reword about page

--- src/About.tsx
+++ src/About.tsx
@@ -12,34 +12,35 @@ function About() {
     <>
       <NavBar />
       <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pt: 5 }}>
-        <Typography variant="h1">Web Dev Talks</Typography>
-        <Typography>{t("aboutUs.quote")}<b>{t("aboutUs.adrianReyes")}</b></Typography>
+        <Typography variant="h1" sx={{ mb: 3 }}>Web Dev Talks</Typography>
+        <Typography sx={{ my: 3 }}>{t("aboutUs.quote")}<b>{t("aboutUs.adrianReyes")}</b></Typography>
 
-        <Avatar variant="square" src={image1} alt="" sx={{ width: '100%', height: '100%', my: 5 }} />
+        <Avatar variant="square" src={image1} alt="" sx={{ width: '100%', height: '100%' }} />
 
-        <Typography variant="h2">{t("aboutUs.whoIs")}</Typography>
+        <Typography variant="h2" sx={{ my: 5 }}>{t("aboutUs.whoIs")}</Typography>
         <Typography>{t("aboutUs.description")}</Typography>
         <Typography>{t("aboutUs.description2")}</Typography>
-        <Typography variant="h2">{t("aboutUs.motivationHeader")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("aboutUs.motivationHeader")}</Typography>
         <Typography>{t("aboutUs.motivationBody1")}</Typography>
         <Typography>{t("aboutUs.motivationBody2")}</Typography>
-        <Typography variant="h2">{t("aboutUs.dynamic")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("aboutUs.dynamic")}</Typography>
         <Typography>{t("aboutUs.dynamicBody1")}</Typography>
+        <Typography>{t("aboutUs.dynamicBody2")}</Typography>
 
         <Avatar variant="square" src={image2} alt="" sx={{ width: '100%', height: '100%', my: 5 }} />
 
         <Avatar variant="square" src={image3} alt="" sx={{ width: '100%', height: '100%', my: 5 }} />
 
-        <Typography variant="h2">{t("aboutUs.socialImpact")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("aboutUs.socialImpact")}</Typography>
         <Typography>{t("aboutUs.socialImpactBody1")}</Typography>
-        <Typography variant="h2">{t("aboutUs.jobsOffer")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("aboutUs.jobsOffer")}</Typography>
         <Typography>{t("aboutUs.jobsOfferBody1")}</Typography>
-        <Typography variant="h2">{t("sponsorship.sponsor")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("sponsorship.sponsor")}</Typography>
         <Typography>{t("aboutUs.sponsorBody")}</Typography>
-        <Typography variant="h2">{t("aboutUs.offer")}</Typography>
+        <Typography variant="h2" sx={{ my: 3 }}>{t("aboutUs.offer")}</Typography>
         <Typography>{t("aboutUs.offerRights")}</Typography>
 
-        <List>
+        <List sx={{ width: '100%' }}>
           <ListItem>
             <ListItemText primary={t('aboutUs.socialMentions')}/>
           </ListItem>
@@ -60,9 +61,9 @@ function About() {
         <Typography>{t("aboutUs.note")}</Typography>
         <Typography>{t("aboutUs.benefitsNote")}</Typography>
         <Typography>{t("aboutUs.closingNote")}</Typography>
-        <Typography variant="h3">{t("aboutUs.communityHeader")}</Typography>
 
-        <Avatar variant="square" src={image4} alt="" sx={{ width: '100%', height: '100%', my: 5 }} />
+        <Typography variant="h3" sx={{ my: 3 }}>{t("aboutUs.communityHeader")}</Typography>
+
       </Container>
     </>
   )
