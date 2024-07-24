22040e38ef79a2a32fdfb007a35f241ef2e0e7b9
Miguel Urbina <itismeurbi@gmail.com>
2024-07-24 10:26:49 -0600

    Add support for url paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     base: env.VITE_BASE_URL,
-    plugins: [react()],
+    plugins: [
+      react(),
+      {
+        name: 'copy-404',
+        closeBundle() {
+          copyFileSync(resolve('404.html'), resolve('dist/404.html'));
+        },
+      },
+    ],
   }
 })
